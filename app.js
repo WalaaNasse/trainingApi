9781a05df52d2eef72a9734c088ce5dc1ef7b455
WalaaNasse <walaanasser26W@gmail.com>
2023-11-08 13:04:47 +0200

    second commit

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ const PORT = process.env.port || 3000;
 app.get('/', function (req, res) {
   res.send('walaa nasser is Now Online')
 })
+app.get('/students', function (req, res) {
+    var data=[
+        {id:1,name:Cezar},
+        {id:2,name:Ibrahim},
+        {id:3,name:Walaa}
+    ]
+    res.send('walaa nasser is Now Online')
+  })
 
 app.listen(PORT)
